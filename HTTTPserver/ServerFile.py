3bfe5391373352804afffe5115b6b0a76e5a9550
toloc <tolochek.dev@mail.ru>
2022-03-20 17:55:54 +0300

    Версия 1.0

--- HTTTPserver/ServerFile.py
+++ HTTTPserver/ServerFile.py
@@ -2,7 +2,6 @@ import os.path
 import sys
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-from sys import path
 from pandas.errors import ParserError
 from QueryPreparation.WorkingWithQuery import *
 from GlobalLogger.Logger import *
@@ -18,6 +17,27 @@ def query_decode(httpreq):
     return httpreq.split(" ")[1].split("/")[2]
 
 
+def path_pointer() -> str:
+    """
+    Проверяет, был ли передан путь к файлу с базой данных через аргументы ком. строки.\n
+    Если файл не был передан или получен некорректный путь к файлу, сервер будет работать со старой БД.\n
+    :return: путь к файлу
+    """
+    path = os.path.abspath('DB_holder\\binlist-data.csv')
+    try:
+        file_from_arg = sys.argv[1]
+        if os.path.exists(file_from_arg):
+            path = file_from_arg
+        else:
+            print("Указанного файла по этому адресу не существует. Выбрана внутренняя БД.")
+    except IndexError:
+        print("Не был указан файл в аргументе командной строки. Будет использована старая БД.")
+    return path
+
+
+database = 0
+
+
 class BankCheckerServer(BaseHTTPRequestHandler):
     """
     Реализация HTTP-сервера для обработки запросов.
@@ -27,10 +47,11 @@ class BankCheckerServer(BaseHTTPRequestHandler):
         """
         Реализация ответа на Get запрос.\n
         """
+        global database
         input_number = query_decode(self.requestline)
         check_input_number = query_validator(input_number)
         if check_input_number:
-            answer = make_json(dataBase.search_bank_by_value(int(str(input_number)[:6])))
+            answer = make_json(database.search_bank_by_value(int(str(input_number)[:6])))
             self.send_response(200, "Answer is ready")
             self.send_header('content-type', 'application/json')
             self.end_headers()
@@ -39,21 +60,32 @@ class BankCheckerServer(BaseHTTPRequestHandler):
             self.send_response(500, "Incorrect Number")
 
 
-# Если не было введено других аргументов при запуске программы, будет использована внутренняя БД.
-file_from_arg = sys.argv[0]
-if os.path.exists(file_from_arg):
-    path = file_from_arg
-else:
-    print("Указанного файла по этому адресу не существует. Выбрана внутренняя БД.")
-    path = os.path.abspath('..\\DB_holder\\binlist-data.csv')
-print(sys.argv)
-dataBase = DataBaseHolder(path)
-try:
-    dataBase.parse_file()
-except ParserError:
-    print("Произошла ошибка в ходе чтения файла.\nБудет использована внутренняя БД.")
-    path = os.path.abspath('..\\DB_holder\\binlist-data.csv')
-    dataBase = DataBaseHolder(path)
-    dataBase.parse_file()
-serv = HTTPServer(("localhost", 80), BankCheckerServer)
-serv.serve_forever()
+def prestart_action():
+    """
+    Парсит файл базы данных и в случае неудачного парсинга выбирает старую базу данных.
+    """
+    global database
+    database = DataBaseHolder(path_pointer())
+    try:
+        database.parse_file()
+    except ParserError:
+        print("Произошла ошибка в ходе чтения файла.\nБудет использована внутренняя БД.")
+        path = os.path.abspath('DB_holder\\binlist-data.csv')
+        database = DataBaseHolder(path)
+        database.parse_file()
+
+
+def start_server(ip: str = 'localhost', port: int = 80):
+    """
+    Запуск сервера
+    :param ip: адрес сервера
+    :param port: порт
+    """
+    prestart_action()
+    serv = HTTPServer((ip, port), BankCheckerServer)
+    print(f"Запущен сервер. Адрес: {ip}. Порт: {port}.")
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("Сервер заершает свою работу по запросу!")
+        serv.shutdown()
---
+++ Main.py
@@ -0,0 +1,7 @@
+from DB_holder.DB_holder import *
+from GlobalLogger.Logger import *
+from HTTTPserver.ServerFile import *
+from QueryPreparation.WorkingWithQuery import *
+
+if __name__ == '__main__':
+    start_server()
